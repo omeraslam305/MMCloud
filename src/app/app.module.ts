c5b5e4ca5a7264dd25d1957e464e660810d26d61
Muhammad Omer Aslam <md.omeraslam@gmail.com>
2018-09-24 12:56:32 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { AmazingTimePickerModule } from 'amazing-time-picker';
     DashboardComponent,
     AddMediaNewsComponent,
     MediaReportsComponent
+    
   ],
   entryComponents: [
     SnakbarComponent
